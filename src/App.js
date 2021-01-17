1f225453be015bb194db5224687578088a99e2d4
Cameron St. Amant <cameronstamant@protonmail.com>
2021-01-16 18:44:06 -0700

    remove boilerplate code add map image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello!</p>
+      <img src="/images/World_map_with_four_colours.svg" alt="world_map"></img>
     </div>
   );
 }
