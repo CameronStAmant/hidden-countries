bda9f03406fa8c3fe0ef1b28a47c3b59da85d130
Cameron St. Amant <cameronstamant@protonmail.com>
2021-01-20 09:46:19 -0700

    states are targetable create menubar structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import { ReactSVG } from 'react-svg';
-import Card from './components/Card';
+import React, { useState } from 'react';
+import MenuBar from './components/MenuBar';
 
 function App() {
-  const getState = (e) => {
-    if (e.target.id !== 'svg' && e.target.id !== '') {
-      console.log(e.target.attributes['data-name'].value);
+  const [lastState, setLastState] = useState('');
+
+  const getState = (selectedState) => {
+    if (selectedState.target.id !== 'svg' && selectedState.target.id !== '') {
+      if (lastState !== '') {
+        lastState.target.style.fill = '#f9f9f9';
+      }
+      selectedState.target.style.fill = 'plum';
+      setLastState(selectedState);
     }
   };
 
   return (
-    <div className="hiddenPopper">
-      <p>Hello!</p>
-      <ReactSVG onClick={getState} src="/images/us.svg" />
-      <Card />
+    <div>
+      <MenuBar />
+      <div className="hiddenPopper" onClick={getState}>
+        <ReactSVG src="/images/us.svg" />
+      </div>
     </div>
   );
 }
---
+++ src/components/MenuBar.js
@@ -0,0 +1,15 @@
+import './MenuBar.css';
+
+const menuBar = () => {
+  return (
+    <div className="menu">
+      <div className="navItems">
+        <div className="leftNav">Find That State!</div>
+        <div className="centerNav">timer</div>
+        <div className="rightNav">leaderboards</div>
+      </div>
+    </div>
+  );
+};
+
+export default menuBar;
